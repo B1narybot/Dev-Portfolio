afc254413eb9972989b20bbc1bf0b65ab22aa398
Tounga-Saidou <23saiodu@gmail.com>
2024-11-22 10:45:14 +0200

    CypressUpdate

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,18 +1,21 @@
 describe('empty spec', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
-  })
+  it('displays the main heading', () => {
+    // Check that the h1 element exists and contains the text "ABOUT"
+    cy.get('h1')
+      .should('contain', 'ABOUT')
+      .and('be.visible');
+  });
+
   it('renders the image', () => {
+    // Check that the image is visible and has a valid natural width
     cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-  
-})
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
